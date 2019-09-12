d99a9d1478fd46cfe96f2b94c04979468522c192
GAPUR EVLOEV <g.evloev@itfsystems.ru>
2019-09-12 16:19:28 +0300

    околофинальный

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-    svg4everybody({});
+    $('.discover__btn').click(function () {
+        $(this).parent().toggleClass('discover--active');
+        $('.questions').toggleClass('questions--active');
+    });
+    $('#burger').click(function () {
+        $('#modal-nav-overlay').toggleClass('overlay-active');
+        $('#modal-nav').toggleClass('modal-nav--active');
+    });
+    $('#modal-close').click(function () {
+        $('#modal-nav-overlay').toggleClass('overlay-active');
+        $('#modal-nav').toggleClass('modal-nav--active');
+    });
 });
